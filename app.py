3dcf0e09bd5ef95359986e412689700038ccf960
Raphaël <raph.robert974@gmail.com>
2021-05-27 11:39:42 +0200

    🏗️  change code archi

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from apps.callbacks import *
 
 ssl._create_default_https_context = ssl._create_unverified_context
 app.title = "Corse Matin Analysis"
-
+server = app.server
 app.layout = html.Div(
     children=[
         html.Div(
--- dash_app.py
+++ dash_app.py
@@ -26,4 +26,3 @@ external_stylesheets = [
     },
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
-server = app.server
